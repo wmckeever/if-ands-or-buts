ad43ee5dda84b1d7165f6aa5c8382f917c08381e
wmckeever <wil.mckeever@gmail.com>
2022-09-19 11:36:55 -0400

    added a string checker and cleaned up the code.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,8 +6,11 @@ console.log(`Player one chose: ${choice1}.`)
 let choice2 = prompt('Player two, please choose rock, paper, or scissors: ');
 console.log(`Player two chose ${choice2}.`)
 
+
+
 if (choice1 === "rock" && choice2 === "paper"){
   console.log('Paper beats Rock. Player Two wins!');
+
 
 }else if (choice1 === "rock" && choice2 === "scissors"){
 console.log('Rock beats Scissors, Player One wins!'); 
@@ -25,8 +28,16 @@ console.log('Rock beats Scissors, Player One wins!');
 }else if(choice1 === "scissors" && choice2 === "rock"){
   console.log('Rock beates Scissors, Player Two wins!')  
 
-
-}else{
+}else if(choice1 && choice2 === "rock"){
   console.log('This game has ended in a draw!')
+
+}else if(choice1 && choice2 === "paper"){
+  console.log('This game has ended in a draw!')
+
+}else if(choice1 && choice2 === "scissors"){
+  console.log('This game has ended in a draw!')
+
+} else if (choice1 !== typeof "string" || choice2 !== typeof "string"){
+  console.log("Sorry, numbers are not allowed. Please choose rock, paper or scissors. ")
 
 }
