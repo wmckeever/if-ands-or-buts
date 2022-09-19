4beae775c7aa3359b47cf0c5f3b26e0172858c1a
wmckeever <wil.mckeever@gmail.com>
2022-09-19 10:08:58 -0400

    Fixed issue where code was stoppping at first if statement.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,10 +13,28 @@ console.log(`Player one chose: ${choice1}.`)
 let choice2 = Number(prompt('Player two, please choose 1. rock, 2. paper, or 3. scissors: '));
 console.log(`Player two chose ${choice2}.`)
 
-if (choice1 && choice2){
-  console.log('Paper beats Rock. Player two wins!')
-}else if(choice2 && choice3){
-  console.log('Scissors beats Paper, Player 3 wins!')
-}else if(choice3 && choice1){
-  console.log('Rock beats Scissors, Player 1 wins!')
+if (choice1 === 1 && choice2 === 2){
+  console.log('Paper beats Rock. Player Two wins!');
+
+}else if (choice1 === 1 && choice2 === 3){
+console.log('Rock beats Scissors, Player One wins!'); 
+
+}else if(choice1 === 2 && choice2 === 1){
+  console.log('Paper beats rock, Player One wins!');
+
+}else if(choice1 === 2 && choice2 === 3){
+  console.log('Scissors beats Paper, Player Two wins!');
+
+
+}else if(choice1 === 3 && choice2 === 2){
+  console.log('Scissors beats Paper, Player One wins!')  
+
+}else if(choice1 === 3 && choice2 === 1){
+  console.log('Rock beates Scissors, Player Two wins!')  
+
+
+}else if(choice1 && choice1 || choice2 && choice2 || choice3 && choice3){
+  console.log('This game has ended in a draw!')
+
 }
+
