eb9d8d1d57e2e99de0a86663a3a1f3fefd36b1d7
wmckeever <wil.mckeever@gmail.com>
2022-09-19 09:15:29 -0400

    Added finished rock, paper, scissors game complete.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,22 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-let rock = true;
-let paper = false;
-let scissors = true;
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+let winner = 0;
 
 
-let choice1 = Number(prompt('Player one, please choose rock, paper, or scissors: '));
+let choice1 = Number(prompt('Player one, please choose 1. rock, 2. paper, or 3. scissors: '));
 console.log(`Player one chose: ${choice1}.`)
 
-let choice2 = Number(prompt('Player two, please choose rock, paper, or scissors: '));
+let choice2 = Number(prompt('Player two, please choose 1. rock, 2. paper, or 3. scissors: '));
 console.log(`Player two chose ${choice2}.`)
 
-if (choice1 === true && choice2 === false){
-  console.log('Paper beats rock. Player two wins!')
+if (choice1 && choice2){
+  console.log('Paper beats Rock. Player two wins!')
+}else if(choice2 && choice3){
+  console.log('Scissors beats Paper, Player 3 wins!')
+}else if(choice3 && choice1){
+  console.log('Rock beats Scissors, Player 1 wins!')
 }
